bab4d11b38cde9ae93536fd5b9d2abbb449c94d1
thecat58 <mkike518@gmail.com>
2023-11-13 14:49:14 -0500

    fo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
