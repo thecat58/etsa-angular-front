f74b5be065814c2334bf7b284abcee3b948831de
thecat58 <mkike518@gmail.com>
2024-02-27 13:20:15 -0500

    se su be chect de municipio

--- src/app/pages/rejistro/rejistro.component.ts
+++ src/app/pages/rejistro/rejistro.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { identificacionModel } from 'src/app/shared/models/Identifica.model';
+import { RejisterService } from 'src/app/shared/services/rejister.service';
 
 @Component({
   selector: 'app-rejistro',
@@ -10,19 +12,26 @@ export class RejistroComponent {
   formLogin!: FormGroup;
   fotoURL: string | ArrayBuffer | null = null;
   file = '';
+  identificacion: identificacionModel[]=[];
+  identificaciones: identificacionModel[] = [];
 
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+    private _servicio: RejisterService) {
     this.buildFormLogin();
+  }
+ 
+  ngOnInit(): void {
+    this.obtenerIdentificaciones();
   }
 
   private buildFormLogin() {
     this.formLogin = this.formBuilder.group({
       usuario: ['', Validators.required],
       password: ['', Validators.required],
-      municipio: ['', Validators.required],
+      // municipio: ['', Validators.required],
       correo: ['', [Validators.required, Validators.email]],
-      identificacion: ['', Validators.required],
+      // identificacion: ['', Validators.required],
       apellido: ['', Validators.required],
       foto: ['', Validators.required],
 
@@ -34,11 +43,31 @@ export class RejistroComponent {
     if (this.formLogin.valid) {
       console.log('Formulario válido');
       console.log('Datos del formulario:', this.formLogin.value);
-      // Aquí puedes enviar los datos del formulario al servidor o realizar otras acciones
+      this._servicio.rejsitroUser(this.formLogin.valid).subscribe(
+        response=>{
+          console.log('lo que trajo el bakent', response);
+        },
+        error =>{
+          console.error('erro de bakent', error);
+          
+        }
+      )
     } else {
       console.log('Formulario inválido');
       // Aquí puedes mostrar mensajes de error o realizar otras acciones si el formulario no es válido
     }
+  }
+
+  obtenerIdentificaciones() {
+    this._servicio.traerIdentificacion().subscribe(
+      (identificaciones) => {
+        this.identificaciones = identificaciones;
+        console.log(this.identificaciones);
+      },
+      (error) => {
+        console.error('Error al obtener las identificaciones:', error);
+      }
+    );
   }
   onFotoSeleccionada(event: any) {
     let archivo;
