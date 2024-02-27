4adc95ebdb2d0084756a6f8d47dbf504adf05106
thecat58 <mkike518@gmail.com>
2024-02-27 11:30:46 -0500

    se ahcer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { TallerComponent } from './pages/taller/taller.component';
+import { Error400Component } from './components/error400/error400.component';
+import { PerfilComponent } from './components/perfil/perfil/perfil.component';
+import { RejistroComponent } from './pages/rejistro/rejistro.component';
 
 const routes: Routes = [
   {path: '',component:HomeComponent},
   {path: 'dashboard',component:HomeComponent},
-
+  { path: 'perfil-venta', component: PerfilComponent },
   {path: 'login',component:LoginComponent, pathMatch: "full"},
-  {path: 'taller',component:TallerComponent}
-
+  {path: 'taller',component:TallerComponent},
+  {path: 'rejistro',component:RejistroComponent},
+  { path: '**', component: Error400Component }
   
->>>>>>> 47dc0428bde53dbb6757663dc5e60274cfe5ed2b
+
 ];
 
 @NgModule({
